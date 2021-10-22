5bb34e5ace7bdca0d1212af3d57861819d0944c3
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-10-21 23:37:24 -0300

    FEAT: Created new molecule table row datas

---
+++ src/components/molecules/TableRowDatas.vue
@@ -0,0 +1,23 @@
+<template>
+  <tr>
+    <TableData v-for="data of datas" :key="data" :text="data" />
+  </tr>
+</template>
+
+<script>
+import TableData from '@/components/atoms/TableData'
+
+export default {
+  name: 'TableRowDatas',
+  props: {
+    datas: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    TableData,
+  },
+  setup() {},
+}
+</script>
