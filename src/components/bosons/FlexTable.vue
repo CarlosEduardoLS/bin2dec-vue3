18deb57bb8b51b7ece3c82da23e82d2c12002f68
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-10-21 23:38:08 -0300

    FEAT: Created new boson flex table

---
+++ src/components/bosons/FlexTable.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex justify-center">
+    <BinaryTable :headers="headers" :datas="datas" />
+  </div>
+</template>
+
+<script>
+import BinaryTable from '@/components/organisms/BinaryTable'
+
+export default {
+  props: {
+    headers: {
+      type: Array,
+      required: true,
+    },
+    datas: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    BinaryTable,
+  },
+  setup() {},
+}
+</script>
