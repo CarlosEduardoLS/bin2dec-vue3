771d83f478dcee3015afad79fbd1680d45e40326
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-10-21 23:36:51 -0300

    FEAT: Created new atom table header

---
+++ src/components/atoms/TableHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <th>{{ text }}</th>
+</template>
+
+<script>
+export default {
+  name: 'TableHeader',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {},
+}
+</script>
+
+<style scoped>
+th {
+  margin-top: 30px;
+  border: 1px solid rgb(14, 165, 233);
+  padding: 10px;
+}
+</style>
