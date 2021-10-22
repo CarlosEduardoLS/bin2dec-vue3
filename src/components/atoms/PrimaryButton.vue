ccd321e1c0839839b6d3d97326a400396f32198e
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-10-21 23:36:30 -0300

    FEAT: Created new atom primary button

---
+++ src/components/atoms/PrimaryButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="bg-light-blue-500 p-2 rounded-2xl text-white" @click="handleClick">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'PrimaryButton',
+  emits: ['click'],
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props, { emit }) {
+    function handleClick(v) {
+      emit('click', v)
+    }
+
+    return {
+      handleClick,
+    }
+  },
+}
+</script>
