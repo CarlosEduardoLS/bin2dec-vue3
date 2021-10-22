a1639399b86fa430a90f30eca909c619738f4c91
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-10-21 23:37:03 -0300

    FEAT: Created new atom text span

---
+++ src/components/atoms/TextSpan.vue
@@ -0,0 +1,16 @@
+<template>
+  <span class="text-center mt-3">{{ text }}</span>
+</template>
+
+<script>
+export default {
+  name: 'TextSpan',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {},
+}
+</script>
