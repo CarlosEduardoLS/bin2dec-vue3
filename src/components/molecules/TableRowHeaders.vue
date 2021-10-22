69788dfed64e92191c84e601b126cfaf0d14e45b
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-10-21 23:37:36 -0300

    FEAT: Created new molecule table row headers

---
+++ src/components/molecules/TableRowHeaders.vue
@@ -0,0 +1,23 @@
+<template>
+  <tr>
+    <TableHeader v-for="header of headers" :key="header" :text="header" />
+  </tr>
+</template>
+
+<script>
+import TableHeader from '@/components/atoms/TableHeader'
+
+export default {
+  name: 'TableRowHeaders',
+  components: {
+    TableHeader,
+  },
+  props: {
+    headers: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup() {},
+}
+</script>
