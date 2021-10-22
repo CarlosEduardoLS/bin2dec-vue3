9746711f6cf518206831577d832dd5f30974d277
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-10-21 23:36:41 -0300

    FEAT: Created new atom table data

---
+++ src/components/atoms/TableData.vue
@@ -0,0 +1,24 @@
+<template>
+  <td>{{ text }}</td>
+</template>
+
+<script>
+export default {
+  name: 'TableData',
+  props: {
+    text: {
+      type: [String, Number],
+      required: true,
+    },
+  },
+  setup() {},
+}
+</script>
+
+<style scoped>
+td {
+  margin-top: 30px;
+  border: 1px solid rgb(14, 165, 233);
+  padding: 10px;
+}
+</style>
